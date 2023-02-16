1cd67e00b49a96911b838708e1bd1ddacaf388ae
Piotr Kilczuk <piotr@kilczuk.dev>
2023-02-16 11:36:09 +0100

    Grodzisk -> Idzikowice

--- src/skrj/builder.py
+++ src/skrj/builder.py
@@ -11,14 +11,13 @@ def build_fodts():
     for train_group_definition in train_definitions.registry:
         for train in train_group_definition:
             print(train.category, train.number)
-            text_out = train_group_definition.template.replace("{{ train_category }}", train.category)
-            text_out = text_out.replace("{{ train_number }}", str(train.number))
+            text_out = train_group_definition.template.replace("{{ cat }}", train.category)
+            text_out = text_out.replace("{{ num }}", str(train.number))
             for hour in HOUR_RE.findall(text_out):
                 hour_parsed = datetime.datetime(2023, 1, 1, hour=int(hour[:2]), minute=int(hour[3:]))
                 hour_adjusted = hour_parsed + train.offset
                 hour_adjusted_as_str = hour_adjusted.strftime("%H:%M")
                 text_out = text_out.replace(hour, hour_adjusted_as_str)
-                print(hour, hour_adjusted_as_str)
             with train.fodt_file() as f:
                 f.write(text_out)
 
